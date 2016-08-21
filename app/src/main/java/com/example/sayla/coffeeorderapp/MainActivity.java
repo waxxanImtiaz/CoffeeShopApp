57e55ce83fa9469d9bef39dedfa7afe5987f46ca
Saylani_IT Mass Taining <Saylani_IT Mass Taining>
2016-08-21 15:23:12 +0500

    Modified with two checkboxes

--- app/src/main/java/com/example/sayla/coffeeorderapp/MainActivity.java
+++ app/src/main/java/com/example/sayla/coffeeorderapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.sayla.coffeeorderapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,8 +19,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button bt_decrement;
     TextView totalOrders;
     TextView  showAmmount;
-    private int total = 0;
-    private final int PRICE_PER_ORDER=15;
+    private String whichCbClicked;
+
+    //CHECKBOXES
+    CheckBox normalCoffee;
+    CheckBox specialCoffee;
+
+    private final int PRICE_FOR_SEPCIAL_ORDER=40;
+    private final int PRICE_FOR_NORMAL_ORDER=20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +37,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt_order = (Button)findViewById(R.id.bt_order);
         totalOrders = (TextView)findViewById(R.id.tv_total_orders);
         showAmmount = (TextView)findViewById(R.id.tv_show_ammount);
+        normalCoffee = (CheckBox)findViewById(R.id.normal);
+        specialCoffee = (CheckBox)findViewById(R.id.special);
 
+        whichCbClicked = "no";
 
+        normalCoffee.setOnClickListener(this);
+        specialCoffee.setOnClickListener(this);
         bt_increment.setOnClickListener(this);
         bt_decrement.setOnClickListener(this);
         bt_order.setOnClickListener(this);
@@ -50,8 +64,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.bt_order:
                 totalAmmount = Integer.parseInt(totalOrders.getText().toString());
-                showAmmount.setText(totalAmmount*PRICE_PER_ORDER+"");
+                if(whichCbClicked.equals("no"))
+                {
+                    showDialog("Please choose coffee orders");
+                }
+                else if(whichCbClicked.equals("special"))
+                {
+                    showAmmount.setText(totalAmmount*PRICE_FOR_SEPCIAL_ORDER+"");
+                }
+                else if(whichCbClicked.equals("normal"))
+                {
+
+                    showAmmount.setText(totalAmmount*PRICE_FOR_NORMAL_ORDER+"");
+                }
                 break;
+            case R.id.special:
+                specialCoffee.setChecked(true);
+                normalCoffee.setChecked(false);
+                whichCbClicked = "special";
+                break;
+            case R.id.normal:
+                specialCoffee.setChecked(false);
+                normalCoffee.setChecked(true);
+                whichCbClicked = "normal";
+                break;
+
+
+
         }
     }//end of onClick method..
 
@@ -62,17 +101,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if(flag.equals("+"))
         {
-           // if(!orderShow.equals("0")) {
-               // ++total;
-                temp = Integer.parseInt(orderShow);
-
-            Toast.makeText(getApplicationContext(), "total="+total, Toast.LENGTH_SHORT).show();
-                temp = ++temp;
-                totalOrders.setText(temp + "");
-                Toast.makeText(getApplicationContext(), "Incremented", Toast.LENGTH_SHORT).show();
-//            }else{
-//                totalOrders.setText(++total);
-//            }
+            temp = Integer.parseInt(orderShow);
+            temp = ++temp;
+            totalOrders.setText(temp + "");
         }
         else
             if(flag.equals("-"))
@@ -88,8 +119,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     totalOrders.setText(temp + "");
 
                 }
-                Toast.makeText(getApplicationContext(), "Decrement", Toast.LENGTH_SHORT).show();
             }//end of else if
     }//end of calculate method..
 
+    public void showDialog(String message)
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(message);
+
+        alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
 }
